d5a5140ace72fe79025be2cc9eea8461b16f3f58
Maksym Prykhodko <maksimprikhodkodnepr@gmail.com>
2019-11-03 20:50:17 +0200

    Updates

---
+++ precache-manifest.e5ba79c5613786a2ea4aee10e65197f9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1ef2009a02e5db2abf67bbbb67065d",
+    "url": "/table-component/index.html"
+  },
+  {
+    "revision": "47d6c6b54413c3ee4a12",
+    "url": "/table-component/static/js/2.8c04bce1.chunk.js"
+  },
+  {
+    "revision": "5707dce75d579f3e0490",
+    "url": "/table-component/static/js/main.ad76d472.chunk.js"
+  },
+  {
+    "revision": "b82fbac45421ce689bab",
+    "url": "/table-component/static/js/runtime-main.4d39283f.js"
+  }
+]);
